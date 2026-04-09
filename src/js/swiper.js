a5faa69c54b12a070e7ec32e7db1e2169d053e5b
Kateryna <katyaukr2014@gmail.com>
2026-04-09 14:34:58 +0300

    add

---
+++ src/js/swiper.js
@@ -0,0 +1,60 @@
+import Swiper from 'swiper';
+import { Autoplay, Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+let coreSwiper = null;
+let gallerySwiper = null;
+
+function initSwipers() {
+  const width = window.innerWidth;
+
+  // ---------------- CORE ----------------
+  if (width < 1440 && !coreSwiper) {
+    coreSwiper = new Swiper('.core-swiper', {
+      modules: [Autoplay],
+      slidesPerView: 1.5,
+      spaceBetween: 16,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    });
+  }
+
+  if (width >= 1440 && coreSwiper) {
+    coreSwiper.destroy(true, true);
+    coreSwiper = null;
+  }
+
+  // ---------------- GALLERY ----------------
+  if (!gallerySwiper) {
+    gallerySwiper = new Swiper('.gallery-swiper', {
+      modules: [Navigation, Autoplay],
+      slidesPerView: 1.1,
+      spaceBetween: 16,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+      },
+      navigation: {
+        nextEl: '.gallery-next',
+        prevEl: '.gallery-prev',
+      },
+
+      breakpoints: {
+        1440: {
+          slidesPerView: 2,
+          spaceBetween: 24,
+        }
+      }
+    });
+  }
+}
+
+initSwipers();
+
+window.addEventListener('resize', () => {
+  initSwipers();
+});
